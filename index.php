cf8317c78f6398a757ad12acdc6d8c84594db925
Andjelija Andrejic <andja.anci99@gmail.com>
2022-01-17 14:26:51 +0100

    inicijalizacija pocetne strane

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Pozorisne predstave</title>
+  </head>
+  <body style="background-image: url('zavesa.jpg'); background-repeat: no-repeat; background-position: center;background-size: cover;">
+  <ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link active" href="index.php">Dodavanje</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="predstave.php">Predstave</a>
+  </li> 
+</ul>
+    
+  </div>
+</nav>
+    <div class="container" >
+      <div class="row" >
+        <div class="col-md-12 mt-5">
+          <h1 class="text-center" style="color: #ffcc99">Dodavanje nove predstave</h1>
+          <hr style="height: 12px;background-color: #ffcc99; width: 43%; margin-left:320px;">
+        </div>
+      </div>
